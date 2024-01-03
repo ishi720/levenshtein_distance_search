d7bad8bd73baeb4fc5ea3a5403d2f694d8db536b
ishi720 <ishizuka720@gmail.com>
2024-01-03 20:08:39 +0900

    テストコードの追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('サジェストの個数のテスト', () => {
+    render(<App />);
+    // 特定のクラス名を持つ要素を取得
+    const showSuggestEle = document.getElementsByClassName('keyword');
+   
+    // クラス名が 'keyword' の要素の数を取得
+    const showSuggestCount = showSuggestEle.length;
+
+    // アサーション: 'keyword' クラス名の要素数が期待値と一致するかを確認
+    expect(showSuggestCount).toBe(15);
+  });
+  test('サジェスト結果のテスト(ぴかちう → ピカチュウ)', () => {
+    render(<App />);
+    const searchInput = screen.getByLabelText('ポケモン名');
+    fireEvent.change(searchInput, { target: { value: 'ぴかちう' } });
+
+    // 特定のクラス名を持つ要素を取得
+    const elementsWithKeywordClass = document.getElementsByClassName('keyword');
+   
+    // クラス名が 'keyword' の要素の数を取得
+    const numberOfKeywordElements = elementsWithKeywordClass.length;
+
+    // アサーション: 'keyword' クラス名の要素数が期待値と一致するかを確認
+    expect(numberOfKeywordElements).toBe(15);
+
+    const hitKeyword = elementsWithKeywordClass[0].innerHTML;
+    expect(hitKeyword).toBe("ピカチュウ");
+  });
+  test('サジェスト結果のテスト(てらぱらす → テラパゴス)', () => {
+    render(<App />);
+    const searchInput = screen.getByLabelText('ポケモン名');
+    fireEvent.change(searchInput, { target: { value: 'てらぱらす' } });
+
+    // 特定のクラス名を持つ要素を取得
+    const elementsWithKeywordClass = document.getElementsByClassName('keyword');
+   
+    // クラス名が 'keyword' の要素の数を取得
+    const numberOfKeywordElements = elementsWithKeywordClass.length;
+
+    // アサーション: 'keyword' クラス名の要素数が期待値と一致するかを確認
+    expect(numberOfKeywordElements).toBe(15);
+
+    const hitKeyword = elementsWithKeywordClass[0].innerHTML;
+    expect(hitKeyword).toBe("テラパゴス");
+  });
+
 });
